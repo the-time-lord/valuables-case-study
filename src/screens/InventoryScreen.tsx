d3ae57c751d7ea036686b2449b9f460a0e4cc43b
Daria Katysheva <Daria.Katysheva@Student.HTW-Berlin.de>
2023-02-02 22:28:21 +0100

    Save valuables

--- src/screens/InventoryScreen.tsx
+++ src/screens/InventoryScreen.tsx
@@ -3,19 +3,21 @@ import CardList from '../components/CardList';
 import { Title } from '../components/Title';
 import { RootTabScreenProps } from '../navigation/types';
 import { colors } from '../theme/colors';
-import { valuablesList } from '../utils/valuablesList';
+import { valuablesList } from '../navigation/valuablesList';
+import { useValuableContext } from '../context/ValuableContext';
 
 export default function InventoryScreen({
   navigation,
   route,
 }: RootTabScreenProps<'Inventory'>) {
   const handleAddButtonPress = () => navigation.navigate('AddItem');
+  const { valuables } = useValuableContext();
 
   return (
     <View style={styles.container}>
       <Title onButtonPress={handleAddButtonPress}>{route.name}</Title>
       <View style={styles.cardList}>
-        <CardList data={valuablesList} />
+        <CardList data={valuables} />
       </View>
     </View>
   );
